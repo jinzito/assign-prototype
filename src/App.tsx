d6f6474181fbd8c2472f2d24f7a39d39c460bf54
Evgeny Rybakov <erybakov@exadel.com>
2021-07-17 11:53:21 +0200

    initial commit, create-react-app, redux-toolkit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AssignPortotypePage from "./app/assign-prototype/AssignPortotypePage";
+
+function App() {
+  return (
+    <div className="App">
+      <AssignPortotypePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/assign-prototype/AssignPrototypePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const AssignPrototypePage: React.FC = () => {
+    return null;
+}
+
+export default AssignPrototypePage;
